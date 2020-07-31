3dbbc9fe99856232365dca5f83a69042a373088b
Thais Laure <thais_laure@hotmail.com>
2020-07-31 01:24:43 -0400

    Telas cadastros

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link} from 'react-router-dom';
 import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,10 +7,10 @@ import Button from '../Button';
 
 function Menu() {
   return <nav className="Menu">
-      <a href="/">
+      <Link to="/" >
       <img className="Logo" src={Logo} alt="Aluraflix logo" />
-      </a>
-      <Button as="a" href="/" className="ButtonLink">
+      </Link>
+      <Button as={Link} to="/cadastro/video" className="ButtonLink" >
       Novo Vídeo
       </Button>
   </nav>;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {  BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video'
+import Home from './pages/Home/Home';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} exact />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+    
+    <Route component={() => (<div>Pagina 404</div>)} />
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
